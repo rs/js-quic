ffa38d482f07eb11359abbc02d99f0e3392814a0
Lane Eden <lpertusi@gmail.com>
2025-06-23 16:24:00 -0400

    clean up

--- docker.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-
-set -ex
-
-docker build --platform=linux/amd64 --security-opt seccomp=unconfined -t js-quic-dev .
-docker run --platform=linux/amd64 --security-opt seccomp=unconfined -it --rm -v .:/home/dev/app js-quic-dev
